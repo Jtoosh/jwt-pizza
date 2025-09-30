875fbc6b6bfb1c1e77f8a6fcfdbaef5a566f0b53
James Teuscher <james.teuscher@outlook.com>
2025-09-30 16:29:11 -0600

    Update (ui testing) frontend testing setup progress

--- tests/frontend.spec.ts
+++ tests/frontend.spec.ts
@@ -1,10 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:5173/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/JWT Pizza/);
 });
 
 
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+});
