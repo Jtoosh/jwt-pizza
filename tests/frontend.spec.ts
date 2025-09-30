0d7f5fed3ad05973e60cbaba6830083ee1bfbcbe
James Teuscher <james.teuscher@outlook.com>
2025-09-30 16:42:25 -0600

    Update (ui testing) progress - 29% coverage

--- tests/frontend.spec.ts
+++ tests/frontend.spec.ts
@@ -9,11 +9,27 @@ test('has title', async ({ page }) => {
 
 
 test('login', async ({ page }) => {
+  await page.route('*/**/api/auth', async (route) => {
+  const loginReq = { email: 'd@jwt.com', password: 'a' };
+  const loginRes = {
+    user: {
+      id: 3,
+      name: 'Kai Chen',
+      email: 'd@jwt.com',
+      roles: [{ role: 'diner' }],
+    },
+    token: 'abcdef',
+  };
+  expect(route.request().method()).toBe('PUT');
+  expect(route.request().postDataJSON()).toMatchObject(loginReq);
+  await route.fulfill({ json: loginRes });
+});
+
   await page.goto('http://localhost:5173/');
   
   await page.goto('http://localhost:5173/');
   await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
   await page.getByRole('button', { name: 'Login' }).click();
 });
