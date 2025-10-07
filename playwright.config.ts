ca3346a0bf990908e9b4edf44252755acf4e9bf6
James Teuscher <james.teuscher@outlook.com>
2025-10-06 22:28:38 -0600

    Update (frontend.spec.ts) ui tests from GitHub, update CI pipeline
    Added the starting test code from class github, updated CI pipeline to run tests with coverage

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -36,7 +36,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], viewport: { width: 800, height: 600 } },
     },
   ],
     webServer: {
