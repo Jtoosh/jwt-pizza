8ca4b4a89ca34907df5e89f32ffd6e1df6d026fa
James Teuscher <james.teuscher@outlook.com>
2025-11-15 22:42:08 -0700

    Add (loadTests) loginAndOrder.js load testing script

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,297 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 50, duration: '2m' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+  const jwts = {}
+
+  group('page_1 - https://pizza.jtdevops.click/', function () {
+    response = http.get('https://pizza.jtdevops.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        dnt: '1',
+        'if-modified-since': 'Wed, 29 Oct 2025 14:01:31 GMT',
+        'if-none-match': '"4565a2e20670ff9c7505ac40af95a890"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(8.7)
+
+    response = http.put(
+      'https://pizza-service.jtdevops.click/api/auth',
+      '{"email":"d@jwt.com","password":"diner"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          dnt: '1',
+          origin: 'https://pizza.jtdevops.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+      if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+      }
+    vars['token1'] = jsonpath.query(response.json(), '$.token')[0]
+
+    response = http.options('https://pizza-service.jtdevops.click/api/auth', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'content-type',
+        'access-control-request-method': 'PUT',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(1.6)
+
+    response = http.get('https://pizza-service.jtdevops.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        dnt: '1',
+        'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.options('https://pizza-service.jtdevops.click/api/order/menu', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.get(
+      'https://pizza-service.jtdevops.click/api/franchise?page=0&limit=20&name=*',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          dnt: '1',
+          'if-none-match': 'W/"5c-UrU6FPurLC0JcnOrzddwdfUXFBA"',
+          origin: 'https://pizza.jtdevops.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+
+    response = http.options(
+      'https://pizza-service.jtdevops.click/api/franchise?page=0&limit=20&name=*',
+      null,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'access-control-request-headers': 'authorization,content-type',
+          'access-control-request-method': 'GET',
+          origin: 'https://pizza.jtdevops.click',
+          priority: 'u=1, i',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    sleep(4.6)
+
+    response = http.get('https://pizza-service.jtdevops.click/api/user/me', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        dnt: '1',
+        'if-none-match': 'W/"5d-+dbZMAD5rSY9bDzahgFK5Yptbr0"',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.options('https://pizza-service.jtdevops.click/api/user/me', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(1.5)
+
+    response = http.post(
+      'https://pizza-service.jtdevops.click/api/order',
+      '{"items":[{"menuId":2,"description":"Pepperoni","price":0.0042},{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          dnt: '1',
+          origin: 'https://pizza.jtdevops.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+
+    jwts['jwt1'] = jsonpath.query(response.json(), '$.jwt')[0]
+
+    response = http.options('https://pizza-service.jtdevops.click/api/order', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(1.8)
+
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt": ${jwts['jwt1']}}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          dnt: '1',
+          origin: 'https://pizza.jtdevops.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+
+    response = http.options('https://pizza-factory.cs329.click/api/order/verify', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        origin: 'https://pizza.jtdevops.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+  })
+}
